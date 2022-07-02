9092e1720ca6ccfe96f37d09a3b09e5b2b0d3ba5
fRanaRfs <franarfs.bureau@gmail.com>
2022-07-02 14:33:31 +0200

    Push
    Api Spotify expess done

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.get('/artist-search', (req, res, next) => {
     spotifyApi
     .searchArtists(artist)
     .then((data) => {
-      console.log('The received data from the API: ', data.body.artists.items);
+      //console.log('The received data from the API: ', data.body.artists.items);
       res.render('artist-search-results.hbs', { artist: data.body.artists.items });
     })
     .catch(err => console.log('The error while searching artists occurred: ', err));
@@ -45,10 +45,21 @@ app.get('/albums/:artistId', (req, res, next) => {
     spotifyApi
     .getArtistAlbums(artistId)
     .then((data) => {
-      console.log('The received data from the API: ', data.body.items);
+        console.log(data.body.items)
       res.render('albums.hbs', { album: data.body.items });
     })
     .catch(err => console.log(artistId, 'The error while searching albums occurred: ', err));
 })
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.get('/tracks/:albumId', (req, res, next) => {
+    const {albumId} = req.params
+    console.log(albumId)
+    spotifyApi
+    .getAlbumTracks(albumId)
+    .then((data) => {
+        res.render('view-tracks.hbs', {track: data.body.items})
+        console.log(data.body.items)
+    })
+    .catch(err => console.log('The error while searching albums occurred: ', err));
+}) 
+app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')); 
